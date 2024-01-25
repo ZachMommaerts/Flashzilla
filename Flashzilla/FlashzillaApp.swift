4d15a6f93fa1a5de9966821ab5ff34dbe6dbc7b4
ZachMommaerts <zmommaerts@gmail.com>
2024-01-25 11:30:32 -0600

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Zach Mommaerts on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
