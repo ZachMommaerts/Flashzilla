1100a2916620f68da42790ae834dca786f82f34a
ZachMommaerts <zmommaerts@gmail.com>
2024-01-29 18:09:11 -0600

    - Add Card object - Add Card view with gesture to show answer

---
+++ Flashzilla/CardView.swift
@@ -0,0 +1,44 @@
+//
+//  CardView.swift
+//  Flashzilla
+//
+//  Created by Zach Mommaerts on 1/29/24.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let card: Card
+    
+    @State private var isShowingAnswer = false
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 25, style: .continuous)
+                .fill(.white)
+                .shadow(radius: 10)
+            
+            VStack{
+                Text(card.prompt)
+                    .font(.largeTitle)
+                    .foregroundStyle(.black)
+                
+                if isShowingAnswer {
+                    Text(card.answer)
+                        .font(.title)
+                        .foregroundStyle(.gray)
+                }
+            }
+            .padding()
+            .multilineTextAlignment(.center)
+        }
+        .frame(width: 450, height: 250)
+        .onTapGesture {
+            isShowingAnswer.toggle()
+        }
+    }
+}
+
+#Preview {
+    CardView(card: Card.example)
+}
