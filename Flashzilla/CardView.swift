93d3e36158862c2ce6e381a808a6bff9db1ead90
zmommaerts <zmommaerts@apple.com>
2024-02-01 11:45:47 -0600

    - Add Haptic feedback when a user gets an answer wrong

--- Flashzilla/CardView.swift
+++ Flashzilla/CardView.swift
@@ -11,6 +11,8 @@ struct CardView: View {
     let card: Card
     var removal: (() -> Void)? = nil
     
+    @State private var feedback = UINotificationFeedbackGenerator()
+    
     @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
     @State private var isShowingAnswer = false
     @State private var offset = CGSize.zero
@@ -54,9 +56,16 @@ struct CardView: View {
             DragGesture()
                 .onChanged { gesture in
                     offset = gesture.translation
+                    if offset.width < 0 {
+                        feedback.prepare()
+                    }
                 }
                 .onEnded { _ in
                     if abs(offset.width) > 100 {
+                        if offset.width < 0 {
+                            feedback.notificationOccurred(.error)
+                        }
+                        
                         removal?()
                     } else {
                         offset = .zero
