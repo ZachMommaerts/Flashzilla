60545a55d07af64267b2faa0178b90d793244b90
ZachMommaerts <zmommaerts@gmail.com>
2024-01-31 21:32:20 -0600

    - Add feature to restart game and stop timer correctly

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -16,7 +16,7 @@ extension View {
 
 struct ContentView: View {
     @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
-    @State private var cards = Array<Card>(repeating: Card.example, count: 10
+    @State private var cards = Array(repeating: Card.example, count: 10
     )
     
     @State private var timeRemaining = 100
@@ -49,6 +49,15 @@ struct ContentView: View {
                         }
                             .stacked(at: index, in: cards.count)
                     }
+                }
+                .allowsHitTesting(timeRemaining > 0)
+                
+                if cards.isEmpty {
+                    Button("Start Again", action: resetCards)
+                        .padding()
+                        .background(.white)
+                        .foregroundStyle(.black)
+                        .clipShape(.capsule)
                 }
             }
             
@@ -84,7 +93,9 @@ struct ContentView: View {
         }
         .onChange(of: scenePhase) { newPhase in
             if newPhase == .active {
-                isActive = true
+                if cards.isEmpty == false {
+                    isActive = true
+                }
             } else {
                 isActive = false
             }
@@ -93,6 +104,16 @@ struct ContentView: View {
     
     func removeCard(at index: Int) {
         cards.remove(at: index)
+        
+        if cards.isEmpty {
+            isActive = false
+        }
+    }
+    
+    func resetCards() {
+        cards = Array(repeating: Card.example, count: 10)
+        timeRemaining = 100
+        isActive = true
     }
 }
 
